924334cca661bd2dcc1e5b139223de426e6e164d
Andreas Müller <schnitzeltony@googlemail.com>
2016-01-09 23:44:39 +0100

    cgenie: align processor speed
    This is a temporary solution possibly causing flicker caused by accessing the
    character generator RAM not working prperly. Alignment was done by writing a
    simple basic loop and measuring time of execution on emulator and original
    hardware. Hope Colour Genie remains working for long...
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/cgenie/main.c
+++ src/cgenie/main.c
@@ -1305,8 +1305,8 @@ int main(int argc, char **argv)
 		0, tmr_double_to_time(TIME_IN_HZ(50)));
 	clock_timer = tmr_alloc(cgenie_clock, tmr_double_to_time(TIME_IN_MSEC(25)),
 		0, tmr_double_to_time(TIME_IN_MSEC(25)));
-	scan_timer = tmr_alloc(cgenie_scan, tmr_double_to_time(TIME_IN_HZ(7200)),
-		0, tmr_double_to_time(TIME_IN_HZ(7200)));
+	scan_timer = tmr_alloc(cgenie_scan, tmr_double_to_time(TIME_IN_HZ(8000)),
+		0, tmr_double_to_time(TIME_IN_HZ(8000)));
 
 	while (!stop)
 		tmr_run_cpu(cpu, 2216800.0);
